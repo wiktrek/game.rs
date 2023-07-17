8a73d23fe0f8c862f98dc6498daef0a82f6cadfb
wiktrek <wiktor.janiszewski10@gmail.com>
2023-07-17 19:06:26 +0200

    bevy

--- src/functions/mod.rs
+++
@@ -1,4 +0,0 @@
-mod dialogue;
-mod file;
-pub use dialogue::create_dialogue;
-pub use file::*;
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
-mod functions;
-use functions::*;
-fn main() {
-    println!("main.rs");
-    start()
-}
-fn start() {
-    let options = vec!["new game", "load from file"];
-    let a = create_dialogue(options.clone(), "Select Option");
-    if a == 0 {
-        create_file();
-    } else {
-        read_file();
-    }
+use bevy::prelude::*;
 
-    println!("You selected option: {} \n{}", a, options[a]);
+fn main() {
+    App::new().run();
 }
